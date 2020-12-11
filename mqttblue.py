e18a81e7a98655ca5317e4bc97fb3bdc83e18ae0
george <george>
2020-12-10 21:15:06 -0500

    renamed scripts

---
+++ mqttblue.py
@@ -0,0 +1,79 @@
+
+import os, subprocess 
+
+def copy(filename, path):
+    command = f"base64 -w60 {filename} | sed 's/^/        /'"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    buf = proc.stdout.read().decode()
+    proc.wait()
+    return f"""\
+  - become: yes
+    copy:
+      mode: 0755
+      dest: {path}
+      content: !!binary |\n{buf}
+"""
+
+print(f"""\
+---
+  # apt
+
+  - become: yes
+    apt:
+      name:
+      - python3-dbus
+      - mosquitto
+
+  # pip
+
+  - become: yes
+    pip:
+      name:
+      - bluezero
+      - paho-mqtt
+
+  # ble
+
+  - become: yes
+    copy:
+      dest: /etc/dbus-1/system.d/ukBaz.bluezero.conf 
+      content: |
+        <?xml version="1.0" encoding="UTF-8"?>
+        <!DOCTYPE busconfig PUBLIC 
+         "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+         "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+        <busconfig>
+          <policy context="default">
+            <allow own="ukBaz.bluezero"/>
+            <allow send_destination="ukBaz.bluezero"
+                   send_interface="org.freedesktop.DBus.Introspectable"/>
+            <allow send_type="method_call" log="true"/>
+          </policy>
+        </busconfig>
+
+  # code
+
+{copy("mqblue.py", "/usr/local/bin/mqblue")}
+{copy("mqclient.py", "/usr/local/bin/mqclient")}
+
+  # output directory
+
+  - become: yes
+    copy:
+      dest: /lib/systemd/system/mqblue.service
+      content: |
+        [Service]
+        ExecStart=/usr/bin/python3 -u /usr/local/bin/mqblue
+        Restart=on-failure
+        RestartSec=10s
+        [Install]
+        WantedBy=multi-user.target
+
+  - become: yes
+    shell: |
+      sudo systemctl enable mqblue
+      sudo systemctl restart mqblue
+
+""")
+
+
---
+++ mqttsoapy.py
@@ -0,0 +1,66 @@
+
+import os, subprocess 
+
+def copy(filename, path):
+    command = f"base64 -w60 {filename} | sed 's/^/        /'"
+    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+    buf = proc.stdout.read().decode()
+    proc.wait()
+    return f"""\
+  - become: yes
+    copy:
+      mode: 0755
+      dest: {path}
+      content: !!binary |\n{buf}
+"""
+
+print(f"""\
+---
+  - set_fact:
+      fs_path: "{{{{ fs_path | default('/srv') }}}}"
+
+  # apt
+
+  - become: yes
+    apt:
+      name:
+      - mosquitto
+
+  # pip
+
+  - become: yes
+    pip:
+      name:
+      - paho-mqtt
+
+  # code
+
+{copy("mqsoapy.py", "/usr/local/bin/mqsoapy")}
+{copy("mqclient.py", "/usr/local/bin/mqclient")}
+
+  # output directory
+
+  - become: yes
+    file: path={{{{ fs_path }}}} state=directory owner={{{{ user }}}} group={{{{ user }}}}
+
+  - become: yes
+    copy:
+      dest: /lib/systemd/system/mqsoapy.service
+      content: |
+        [Service]
+        ExecStart=/usr/bin/python3 -u /usr/local/bin/mqsoapy --pause --output {{{{ fs_path }}}}/out
+        # WorkingDirectory={{{{ fs_path }}}}
+        User={os.environ['USER']}
+        Restart=on-failure
+        RestartSec=10s
+        [Install]
+        WantedBy=multi-user.target
+
+  - become: yes
+    shell: |
+      sudo systemctl enable mqsoapy
+      sudo systemctl restart mqsoapy
+
+""")
+
+
